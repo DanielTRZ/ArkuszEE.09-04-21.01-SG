33cb4609a6aae3d5a6d13503a8fb6601e23de7e2
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2022-01-17 17:50:36 +0100

    Add files via upload

--- waga.php
+++ waga.php
@@ -7,7 +7,35 @@
     
 </head>
 <body>
+   <div id="baner"><h2>Oblicz wzkaźnik BMI</h2></div>
+   <div id="logo"><img src="wzor.png" alt="liczymy BMI"></div>
+   <div id="lewy"><img src="rys1.png" alt="zrzuć kalorie!"></div>
+   <div id="prawy">
+   <h1>Podaj dane</h1>
+   <form  action="waga.php" method="post" name="waga" id="waga">
+      
+      Waga<input type="number">
+      Wzrost[cm]<input type="number">
+      <button type="submit">Licz BMI i zapisz wynik</button>
+       
+       <?php
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       ?>
+       
+   </form>
    
+   </div>
+   <div id="glowny"></div>
+   <div id="stopka"></div>
     
     
     
