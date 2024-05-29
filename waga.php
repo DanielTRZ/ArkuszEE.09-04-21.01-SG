b8db0b529466e7164c3bb488f8f770f0115a3ef1
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-05-29 21:05:19 +0200

    Update waga.php

--- waga.php
+++ waga.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Twój wskaźnik BMI</title>
-    <link rel="stylesheet" href="styl4.css"> 
+<link rel="stylesheet" href="styl4.css"> 
 </head>
 <body>
    <div id="baner"><h2>Oblicz wzkaźnik BMI</h2></div>
